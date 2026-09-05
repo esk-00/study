6938187e9f4aefda688158f311039f06088cb46f
esk-00 <zeroikku@gmail.com>
2026-09-05 23:38:16 +0900

    quickstart

---
+++ quantom/qiskit/quickstart/circuit.py
@@ -0,0 +1,20 @@
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+# Build your circuit
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
+
+sampler = StatevectorSampler()
+result = sampler.run([qc], shots=1024).result()
+print(result[0].data.meas.get_counts())
+
+# Visualize your results
+counts = result[0].data.meas.get_counts()
+plot_histogram(counts)
+
+plt.show()
